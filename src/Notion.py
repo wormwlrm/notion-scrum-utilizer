6a403c4e44a10292b19616a873dc7a09247cdc25
Jongyoon Jeong <wormwlrm@naver.com>
2021-05-16 18:14:05 +0900

    feat: 스크럼보드로 리네이밍

---
+++ src/Notion.py
@@ -0,0 +1,29 @@
+import os
+from notion.client import NotionClient
+from dotenv import load_dotenv
+import yaml
+
+
+class Notion:
+    client = None
+    token = None
+    conf = None
+    scrum = None
+
+    def __init__(self):
+        self.set_config()
+        self.set_token()
+        self.set_scrum()
+
+    def set_config(self):
+        with open('config.yaml') as file:
+            self.conf = yaml.load(file, Loader=yaml.BaseLoader)
+
+    def set_token(self):
+        load_dotenv(verbose=True)
+        self.token = os.getenv('token_v2')
+        self.client = NotionClient(token_v2=self.token)
+
+    def set_scrum(self):
+        url = self.conf['SCRUM']['URL']
+        self.scrum = self.client.get_collection_view(url)
