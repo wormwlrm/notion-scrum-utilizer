6a97d430747fa6ecd2ce564ee370997f0a6a1bd9
Jongyoon Jeong <wormwlrm@naver.com>
2021-05-23 17:45:38 +0900

    feat: 날짜 자동 설정 옵션 추가

--- daily.py
+++ daily.py
@@ -1,7 +1,10 @@
 from main import scrum
 from run_auto_daily_reset import run_auto_daily_reset
-from update_finished_time import update_finished_time
+from run_auto_update_duration import run_auto_update_duration
 
 if scrum.conf['OPTIONS']['AUTO_DAILY_RESET']:
     print(run_auto_daily_reset())
-    print(update_finished_time())
+
+if scrum.conf['OPTIONS']['AUTO_UPDATE_DURATION']:
+    print(run_auto_update_duration())
+
---
+++ run_auto_update_duration.py
@@ -0,0 +1,50 @@
+from main import scrum
+from datetime import date
+from notion.collection import NotionDate
+
+
+def run_auto_update_duration():
+    return update_done_card() and update_doing_card()
+
+
+def update_done_card():
+    key = scrum.conf["CARD"]["STATUS"]
+    done = scrum.conf["SCRUM"]["STEPS"]["DONE"]
+    duration = scrum.conf["CARD"]["DURATION"]
+
+    try:
+        today = date.today()
+
+        for card_id in scrum.get_filtered_card_ids(key, done):
+            card = scrum.client.get_block(card_id)
+
+            notion_date = card.get_property(duration)
+            if (not notion_date) or (notion_date.start == None):
+                notion_date = NotionDate(start=today)
+            notion_date.end = today
+
+            card.set_property(duration, notion_date)
+        return True
+    except:
+        return False
+
+
+def update_doing_card():
+    status = scrum.conf["CARD"]["STATUS"]
+    doing = scrum.conf["SCRUM"]["STEPS"]["DOING"]
+    duration = scrum.conf["CARD"]["DURATION"]
+
+    try:
+        today = date.today()
+
+        for card_id in scrum.get_filtered_card_ids(status, doing):
+            card = scrum.client.get_block(card_id)
+
+            notion_date = card.get_property(duration)
+            if (not notion_date) or (notion_date.start == None):
+                notion_date = NotionDate(start=today)
+
+            card.set_property(duration, notion_date)
+        return True
+    except:
+        return False
