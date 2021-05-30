497ee20545cb65c5cf9fe18418ab999def9f8ced
Jongyoon Jeong <wormwlrm@naver.com>
2021-05-30 16:08:59 +0900

    fix: 오토 업데이트 날짜 수정

--- run_auto_update_duration.py
+++ run_auto_update_duration.py
@@ -17,11 +17,16 @@ def update_done_card():
 
         for card_id in scrum.get_filtered_card_ids(key, done):
             card = scrum.client.get_block(card_id)
-
             notion_date = card.get_property(duration)
-            if (not notion_date) or (notion_date.start == None):
+
+            if not notion_date:
+                notion_date = NotionDate(start=today, end=today)
+
+            if notion_date.start == None:
                 notion_date = NotionDate(start=today)
-            notion_date.end = today
+
+            if notion_date.end == None:
+                notion_date = NotionDate(start=notion_date.start, end=today)
 
             card.set_property(duration, notion_date)
         return True
